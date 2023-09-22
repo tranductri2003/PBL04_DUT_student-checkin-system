6096b754b9f2c355dbfdf0a30f087e9f7fe2e816
tranductri2003 <89126960+tranductri2003@users.noreply.github.com>
2023-09-22 11:34:16 +0700

    Update settings.py, add necessary lib

--- BackEnd/core/urls.py
+++ BackEnd/core/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
+    # Admin page URLs
     path('admin/', admin.site.urls),
+ 
 ]
