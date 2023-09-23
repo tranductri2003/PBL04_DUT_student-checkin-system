829467e301042e2b3d01fb44e1f5fa89d51fab95
tranductri2003 <89126960+tranductri2003@users.noreply.github.com>
2023-09-23 13:46:04 +0700

    Update user api

--- BackEnd/users/urls.py
+++ BackEnd/users/urls.py
@@ -8,15 +8,15 @@ app_name = 'users'
 urlpatterns = [
     # Xem danh sách người dùng
     path('', views.UserListView.as_view(), name='user-list'),
-    # path('student/', views.StudentListView.as_view(), name='student-list'),
-    # path('teacher/', views.TeacherListView.as_view(), name='teacher-list'),
-    # path('admin/', views.AdminListView.as_view(), name='admin-list'),
+    path('student/', views.StudentListView.as_view(), name='student-list'),
+    path('teacher/', views.TeacherListView.as_view(), name='teacher-list'),
+    path('admin/', views.AdminListView.as_view(), name='admin-list'),
 
     # # Xem chi tiết người dùng
     # path('<str:staff_id>/', views.UserDetailView.as_view(), name='user-detail'),
 
     # # Tạo, Chỉnh sửa và Xóa người dùng
     path('create/', views.UserCreateView.as_view(), name='user-create'),
-    # path('edit/<str:staff_id>/', views.UserUpdateView.as_view(), name='user-update'),
-    # path('delete/<str:staff_id>/', views.UserDeleteView.as_view(), name='user-delete'),
+    path('edit/<str:staff_id>/', views.UserUpdateView.as_view(), name='user-update'),
+    path('delete/<str:staff_id>/', views.UserDeleteView.as_view(), name='user-delete'),
 ]
