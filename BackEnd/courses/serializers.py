bcd3e4dfb660f8359ed8d293e980fe86a9f6bf33
tranductri2003 <89126960+tranductri2003@users.noreply.github.com>
2023-09-27 12:07:05 +0700

    Update course API: add-student, delete-student, list-student

--- BackEnd/courses/serializers.py
+++ BackEnd/courses/serializers.py
@@ -1,9 +1,15 @@
 # courses/serializers.py
 
 from rest_framework import serializers
-from .models import Courses
+from .models import Courses, StudentCourse
 
-class CoursesSerializer(serializers.ModelSerializer):
+class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Courses
         fields = '__all__'
+
+class StudentCourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StudentCourse
+        fields = '__all__'
+        
