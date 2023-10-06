6368420ac21ef3a177d1da39769b9f0d7865984c
lea.tuan021211 <lea.tuan021211@gmail.com>
2023-10-06 16:18:16 +0700

    Fix query all course and all course today

--- BackEnd/courses/serializers.py
+++ BackEnd/courses/serializers.py
@@ -1,11 +1,20 @@
 from rest_framework import serializers
+from users.models import NewUser
 
 from .models import Courses, UserCourse
 
 class CourseSerializer(serializers.ModelSerializer):
+    teacher_id = serializers.CharField(max_length=255, required=True)
     class Meta:
         model = Courses
         fields = '__all__'
+
+    def create(self, validated_data):
+        teacher_id = validated_data.get('teacher_id')
+        teacher = NewUser.objects.get(staff_id=teacher_id)
+        validated_data['teacher_id'] = teacher
+        course = Courses.objects.create(**validated_data)
+        return course
 
 class UserCourseSerializer(serializers.ModelSerializer):
     class Meta:
