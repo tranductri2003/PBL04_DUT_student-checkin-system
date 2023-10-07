915cfd989b3315fb8beebeacaf16a4991dc47299
Phatdtvdvt <phatanhpham2003@gmail.com>
2023-10-07 10:58:45 +0700

    add view assign_students_to_courses,

--- BackEnd/courses/urls.py
+++ BackEnd/courses/urls.py
@@ -7,6 +7,7 @@ from .views import (
     CourseStudentListView,
     StudentEnrollView,
     StudentDeleteView,
+    AssignStudentsToCoursesView,
 )
 
 app_name = 'courses'
@@ -19,7 +20,7 @@ urlpatterns = [
     path('<str:course_id>', CoursesRetrieveUpdateDeleteView.as_view(), name='courses-retrieve-update-delete'),
     
     # Xem danh sách lớp học phần của một sinh viên
-    path('today/', TodayCoursesListView.as_view(), name='courses-today-list'),
+    path('today/<str:staff_id>', TodayCoursesListView.as_view(), name='courses-today-list'),
     
     # Xem danh sách sinh viên của một lớp học phần
     path('list-student/<str:course_id>/', CourseStudentListView.as_view(), name='course-student-list'),
@@ -29,4 +30,7 @@ urlpatterns = [
     
     # Xóa một sinh viên ra khỏi lớp
     path('delete-student/<str:course_id>/<str:student_id>/', StudentDeleteView.as_view(), name='student-enrollment-delete'),
+    # Gán user cho từng course
+    path('assign_students_to_courses/', AssignStudentsToCoursesView.as_view(), name='assign_students_to_courses'),
+
 ]
--- BackEnd/courses/views.py
+++ BackEnd/courses/views.py
@@ -37,6 +37,7 @@ class CoursesListCreateView(generics.ListCreateAPIView):
             elif role == 'T':
                 query_set = Courses.objects.filter(teacher_id=recent_user)
             elif role == 'A':
+                staffId = self.kwargs['staff_id']
                 query_set = Courses.objects.all()   
         else:
             query_set = Courses.objects.all()
@@ -131,3 +132,24 @@ class StudentDeleteView(generics.DestroyAPIView):
         course.save()
         
         return response
+    
+
+
+class AssignStudentsToCoursesView(generics.CreateAPIView):
+    permission_classes = [permissions.AllowAny]
+    
+    def create(self, request, *args, **kwargs):
+        # Get all users with role 'S'
+        students = NewUser.objects.filter(role='S')
+        
+        # Get all existing courses
+        courses = Courses.objects.all()
+        
+        # Loop through each student and each course to create UserCourse entries
+        for student in students:
+            for course in courses:
+                # Check if the user is already enrolled in the course
+                if not UserCourse.objects.filter(user=student, course=course).exists():
+                    UserCourse.objects.create(user=student, course=course)
+        
+        return Response({"message": "Students assigned to courses successfully"}, status=201)
