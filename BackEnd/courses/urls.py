a18f16a4c4e8476b1bb2ef77b1f29c60da8d74e3
lea.tuan021211 <lea.tuan021211@gmail.com>
2023-10-07 09:31:59 +0700

    Edit Course View

--- BackEnd/courses/urls.py
+++ BackEnd/courses/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from .views import (
     CoursesListCreateView,
     CoursesRetrieveUpdateDeleteView,
-    TodayCoursesListView,
     CourseStudentListView,
     StudentEnrollView,
     StudentDeleteView,
@@ -15,11 +14,8 @@ urlpatterns = [
     # Xem danh sách tất cả khóa học
     path('', CoursesListCreateView.as_view(), name='courses-list-create'),
 
-    # Xem chi tiết khóa học theo course_id
+    # Chỉnh sửa, xóa khóa học theo course_id
     path('<str:course_id>', CoursesRetrieveUpdateDeleteView.as_view(), name='courses-retrieve-update-delete'),
-    
-    # Xem danh sách lớp học phần của một sinh viên
-    path('today/', TodayCoursesListView.as_view(), name='courses-today-list'),
     
     # Xem danh sách sinh viên của một lớp học phần
     path('list-student/<str:course_id>/', CourseStudentListView.as_view(), name='course-student-list'),
