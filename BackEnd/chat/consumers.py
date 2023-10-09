98618c64b81077ea5eca07e384e3c03b94f4013f
tranductri2003 <89126960+tranductri2003@users.noreply.github.com>
2023-10-09 22:58:27 +0700

    update chat

--- BackEnd/chat/consumers.py
+++ BackEnd/chat/consumers.py
@@ -25,7 +25,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
 
         text_data_json = json.loads(text_data)
-        print("\033[91m" + json.dumps(text_data_json), "----------------------\033[0m")
         message = text_data_json["message"]
         staff_id = text_data_json["staff_id"]
         room_name = text_data_json["room_name"]
@@ -50,10 +49,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
     
     @sync_to_async
     def save_message(self, message, staff_id, room_name):
-
-        print("\033[91m" + staff_id, "----------------------\033[0m")
-        print("\033[91m" + room_name, "----------------------\033[0m")
-        print("\033[91m" + message, "----------------------\033[0m")
         
         user=NewUser.objects.get(staff_id=staff_id)
         room=Room.objects.get(slug=room_name)
