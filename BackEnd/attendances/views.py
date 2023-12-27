e19a1d8fb8e2abc5de3e21b9f72afa2c08d1ddfb
tranductri2003 <89126960+tranductri2003@users.noreply.github.com>
2023-12-28 00:01:21 +0700

    Update  env, update views.py (filter for attendances)

--- BackEnd/attendances/views.py
+++ BackEnd/attendances/views.py
@@ -2,6 +2,7 @@ from rest_framework import  generics, permissions
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response    
 from rest_framework import status
+from django.core.exceptions import PermissionDenied
 
 from attendances.models import Attendances
 from attendances.serializers import AttendanceSerializer
@@ -40,7 +41,26 @@ class AttendanceListView(generics.ListAPIView):
         else:
             return Response({"message": "You do not have permission to access this resource."}, status=status.HTTP_403_FORBIDDEN)
        
-        return queryset.order_by('-attendance_date', '-attendance_time')       
+        course_id = self.request.GET.get('course_id', None)
+        if course_id is not None:
+            queryset = queryset.filter(course_id__course_id=course_id)
+        
+        attendance_date = self.request.GET.get('attendance_date', None)
+        if attendance_date is not None:
+            queryset = queryset.filter(attendance_date=attendance_date)
+        
+        attendance_status = self.request.GET.get('status', None)
+        if attendance_status is not None:
+            queryset = queryset.filter(status=attendance_status)
+            
+        
+        student_id = self.request.GET.get('student_id', None)
+        if student_id is not None:
+            if student_id != self.request.user.staff_id:
+                raise PermissionDenied("You do not have permission")
+            else:
+                queryset = queryset.filter(student_id__staff_id=student_id)
+        return queryset.order_by('-attendance_date', '-attendance_time')     
         
 class AttendanceUpdateView(generics.UpdateAPIView):
     permission_classes = [permissions.IsAuthenticated]
