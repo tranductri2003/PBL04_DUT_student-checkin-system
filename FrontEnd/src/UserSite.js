39def506889e1988022efbb3abf0ccabaeed370c
Phatdtvdvt <phatanhpham2003@gmail.com>
2023-12-30 01:08:01 +0700

    update password userSite

--- FrontEnd/src/UserSite.js
+++ FrontEnd/src/UserSite.js
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axiosInstance from './axios';
 import { useParams } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import Modal from 'react-modal';
+import TextField from '@material-ui/core/TextField';
+import { message, notification } from 'antd';
+import jwt_decode from "jwt-decode";
+
 
 import './components/profile/Profile.css'
 
@@ -11,9 +17,27 @@ function getDayOfWeekName(dayOfWeek) {
 function Profile() {
     const [userInfo, setUserInfo] = useState(null);
     const [courseInfo, setCourseInfo] = useState(null);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
 
     const { staffId } = useParams();
+    var user_staff_id = "";
+    var role = "";
+    if (localStorage.getItem('access_token')) {
+        // Lấy token từ nơi bạn lưu trữ nó, ví dụ localStorage hoặc cookies
+        const token = localStorage.getItem("access_token"); // Thay thế bằng cách lấy token từ nơi bạn lưu trữ nó
 
+        // Giải mã token
+        const decodedToken = jwt_decode(token);
+
+        // Lấy staff_id từ payload của token
+        user_staff_id = decodedToken.staff_id;
+        role = decodedToken.role;
+
+        //console.log(role);
+    }
     useEffect(() => {
         axiosInstance.get(`/user/${staffId}`)
             .then(response => {
@@ -94,14 +118,22 @@ function Profile() {
                                 <label htmlFor="sTate" className="boldLabel">Class</label>
                                 <input type="text" id="sTate" placeholder="Lớp sinh hoạt" className="fullLengthControl" value={userInfo.class_id} />
                             </div>
-                            <div style={{ width: '49%' }}>
-                                <label htmlFor="zIp" className="boldLabel">Address</label>
-                                <input type="text" id="zIp" placeholder="Địa chỉ" className="fullLengthControl" />
-                            </div>
+                            {(staffId === user_staff_id || role === 'A') && (
+                                <div style={{ width: '49%' }}>
+                                    <label htmlFor="sTate" className="boldLabel">Update Password</label>
+                                    <Button
+                                        variant="contained"
+                                        color="primary"
+                                        onClick={openModal}
+                                    >
+                                        Update
+                                    </Button>
+                                </div>
+                            )}
                         </div>
-
                     </div>
                 </div>
+                <ForgotPasswordModal isOpen={isModalOpen} onRequestClose={closeModal} staffId={staffId} />
             </div>
 
 
@@ -144,4 +176,128 @@ function Profile() {
     );
 }
 
+function ForgotPasswordModal({ isOpen, onRequestClose, staffId }) {
+    // Trạng thái và hàm xử lý cho form quên mật khẩu
+    const [formData, setFormData] = useState({
+        staff_id: '',
+        email: '',
+    });
+    const [error, setError] = useState('');
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData.new_password, formData.old_password, formData.confirm_new_password)
+        if (formData.new_password !== formData.confirm_new_password) {
+            setError('New password and confirm new password do not match.');
+            return; // Stop the form submission
+        }
+        if (formData.new_password === formData.old_password) {
+            setError('The old password must be different from the new password.');
+            return;
+        }
+        setError('');
+
+        const payload = {
+            old_password: formData.old_password,
+            new_password: formData.new_password,
+        };
+
+        axiosInstance.put(`/user/${staffId}/`, payload)
+            .then(response => {
+                console.log(response.data);
+                notification.success({
+                    message: 'Update Password successfully',
+                    description: `Your password was updated!!!`,
+                    placement: 'topRight'
+                })
+                // Xử lý thành công (Bạn cũng có thể hiển thị thông báo thành công)
+                onRequestClose(); // Đóng modal sau khi gửi yêu cầu
+            })
+            .catch(error => {
+                console.error('Lỗi cập nhật mật khẩu:', error);
+                // Xử lý lỗi (Bạn cũng có thể hiển thị thông báo lỗi)
+            });
+        onRequestClose(); // Đóng modal sau khi gửi
+    };
+
+    return (
+        <Modal isOpen={isOpen}
+            onRequestClose={onRequestClose}
+            style={{
+                overlay: {
+                    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                },
+
+                content: {
+                    top: '50%',
+                    left: '50%',
+                    right: 'auto',
+                    bottom: 'auto',
+                    marginRight: '-50%',
+                    transform: 'translate(-50%, -50%)',
+                    width: '70%', // Tăng kích thước chiều rộng
+                    height: '60%', // Điều chỉnh chiều cao tự động
+                    maxWidth: '600px', // Giới hạn kích thước tối đa
+                }
+            }}
+
+        >
+            <div className="forgotPasswordContainertent">
+                <h2>Update Password</h2>
+                <form className="forgotPasswordForm" onSubmit={handleSubmit}>
+                    <TextField
+                        className="forgotPasswordTextField"
+                        type="password"
+                        name="old_password"
+                        label="Old Password"
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        className="forgotPasswordTextField"
+                        type="password"
+                        name="new_password"
+                        label="New Password"
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        className="forgotPasswordTextField"
+                        type="password"
+                        name="confirm_new_password"
+                        label="Confirm New Password"
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        onChange={handleChange}
+                    />
+                    {error && <div style={{ color: 'red' }}>{error}</div>}
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className="modalButton"
+                    >
+                        Update
+                    </Button>
+                </form>
+            </div>
+        </Modal>
+    );
+}
 export default Profile;
