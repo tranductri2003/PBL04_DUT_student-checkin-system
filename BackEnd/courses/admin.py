99b36f032e7936d6039bd03e3b4864ddcb297868
tranductri2003 <tranductri2003@gmail.com>
2024-01-01 19:32:14 +0700

    Update admin.py to hide un-used fields. Update nginx

--- BackEnd/courses/admin.py
+++ BackEnd/courses/admin.py
@@ -3,14 +3,14 @@ from .models import Courses, UserCourse
 
 @admin.register(Courses)
 class CoursesAdmin(admin.ModelAdmin):
-    list_display = ('course_id', 'course_name','num_of_student', 'teacher_id', 'day_of_week', 'start_time', 'end_time', 'start_date', 'end_date', 'room', 'created_at', 'updated_at')
+    list_display = ('course_id', 'course_name','num_of_student', 'teacher_id', 'day_of_week', 'start_time', 'end_time', 'room', 'created_at', 'updated_at')
     list_filter = ('day_of_week', 'start_date', 'end_date', 'created_at', 'updated_at')
     search_fields = ('course_id', 'course_name', 'teacher_id__full_name', 'room')
     date_hierarchy = 'created_at'
 
     fieldsets = (
         (None, {
-            'fields': ('course_id', 'course_name','num_of_student', 'teacher_id', 'day_of_week', 'start_time', 'end_time', 'start_date', 'end_date', 'room')
+            'fields': ('course_id', 'course_name', 'teacher_id', 'day_of_week', 'start_time', 'end_time', 'room')
         }),
     )
     readonly_fields = ('created_at', 'updated_at')
