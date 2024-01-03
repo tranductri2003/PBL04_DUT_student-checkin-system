f6d5ac69914434e6e5f9bde0b5aaf0c41b6ed8a8
tranductri2003 <tranductri2003@gmail.com>
2024-01-03 19:17:29 +0700

    Update admin.py of courses

--- BackEnd/courses/admin.py
+++ BackEnd/courses/admin.py
@@ -3,7 +3,7 @@ from .models import Courses, UserCourse
 
 @admin.register(Courses)
 class CoursesAdmin(admin.ModelAdmin):
-    list_display = ('course_id', 'course_name','num_of_student', 'teacher_id', 'day_of_week', 'start_time', 'end_time', 'room', 'created_at', 'updated_at')
+    list_display = ('course_id', 'course_name', 'teacher_id', 'day_of_week', 'start_time', 'end_time', 'room', 'created_at', 'updated_at')
     list_filter = ('day_of_week', 'start_date', 'end_date', 'created_at', 'updated_at')
     search_fields = ('course_id', 'course_name', 'teacher_id__full_name', 'room')
     date_hierarchy = 'created_at'
